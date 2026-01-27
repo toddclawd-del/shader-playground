ecb2cc92069b5fb7a97b56aa9e5cd93bfcfa8e62
toddclawd-del <toddclawd@gmail.com>
2026-01-27 09:50:29 -0700

    revert: back to single-page, routing not needed yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { ShaderViewer } from './pages/ShaderViewer'
+import { useEffect } from 'react'
+import { Leva } from 'leva'
+import { ShaderCanvas } from './components/ShaderCanvas'
+import { ShaderInfo } from './components/ShaderInfo'
+import { useDynamicControls } from './hooks/useDynamicControls'
+import { useShaderStore } from './stores/shaderStore'
+import { shaderRegistry } from './shaders'
+
+function Controls() {
+  useDynamicControls()
+  return null
+}
 
 function App() {
+  const { currentShaderId, setCurrentShader } = useShaderStore()
+  
+  // Initialize with first shader
+  useEffect(() => {
+    if (!currentShaderId) {
+      const firstShaderId = Object.keys(shaderRegistry)[0]
+      const firstConfig = shaderRegistry[firstShaderId]
+      if (firstShaderId && firstConfig) {
+        setCurrentShader(firstShaderId, firstConfig)
+      }
+    }
+  }, [currentShaderId, setCurrentShader])
+  
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:shaderId" element={<ShaderViewer />} />
-      </Routes>
-    </HashRouter>
+    <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
+      <Leva
+        collapsed={false}
+        flat={false}
+        theme={{
+          colors: {
+            elevation1: '#1a1a2e',
+            elevation2: '#16213e',
+            elevation3: '#0f3460',
+            accent1: '#e94560',
+            accent2: '#ff6b6b',
+            accent3: '#4ecdc4',
+          },
+        }}
+      />
+      <Controls />
+      <ShaderCanvas />
+      <ShaderInfo />
+    </div>
   )
 }
 
